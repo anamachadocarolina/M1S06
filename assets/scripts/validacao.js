67b2493837785daa50ec0a1f5151dbff1facd058
anamachadocarolina <acarolina066@gmail.com>
2024-03-07 14:18:37 -0300

    Adiciona mudança dinâmia no nome

--- assets/scripts/validacao.js
+++ assets/scripts/validacao.js
@@ -1,4 +1,15 @@
+let nome = document.getElementById("nome");
+
 
 function bemvindo(){
     alert('Seja bem vindo!');
 }
+
+
+nome.addEventListener("mouseover", function(event) {
+    nome.textContent = 'Ana Machado';
+});
+
+nome.addEventListener("mouseleave", function(event){
+    nome.textContent = 'Ana Carolina de Oliveira Machado';s
+});
