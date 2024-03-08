6235b4413d2bbc41ed76fbf8a32131e41e63fad1
anamachadocarolina <acarolina066@gmail.com>
2024-03-08 14:02:43 -0300

    Adiciona animações em imagens e links para contatos

--- assets/scripts/validacao.js
+++ assets/scripts/validacao.js
@@ -1,4 +1,6 @@
 let nome = document.getElementById("nome");
+let contato = document.getElementById('contato');
+let email = document.getElementById('email');
 
 
 function bemvindo(){
@@ -12,4 +14,20 @@ nome.addEventListener("mouseover", function(event) {
 
 nome.addEventListener("mouseleave", function(event){
     nome.textContent = 'Ana Carolina de Oliveira Machado';s
+});
+
+contato.addEventListener("mouseover", function(event) {
+    contato.textContent = 'Click para iniciar uma conversa por whatsapp';
+});
+
+contato.addEventListener("mouseleave", function(event){
+    contato.textContent = '(91) 9 9999 - 9999';
+});
+
+email.addEventListener("mouseover", function(event) {
+    email.textContent = 'Click para enviar um email';
+});
+
+email.addEventListener("mouseleave", function(event){
+    email.textContent = 'acarolina066@gmail.com';
 });
